1965d43ac695bba2664b427aea5aff210407ca60
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-16 16:25:59 +0900

    Update server.js

--- server.js
+++ server.js
@@ -66,5 +66,5 @@ io.on("connection", (socket) => {
 // サーバーの起動
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+    console.log(`listening on http://localhost:${PORT}`);
 });
