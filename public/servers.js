348d662f7c021b7b0b1230830a5706c8d8df3614
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-16 16:50:25 +0900

    Update servers.js

--- public/servers.js
+++ public/servers.js
@@ -36,6 +36,38 @@ socket.on("update users count", function (count) {
   updateUsersCount(count);
 });
 
+// 前のカウント要素を格納するための変数
+let countElement = null;
+
+socket.on("count message", function (counts) {
+  console.log(counts); // デバッグ用にコンソールに表示
+
+  // 新しいカウント要素を作成
+  const newElement = document.createElement("div");
+  newElement.textContent = "Please wait... " + counts;
+  newElement.style.position = "fixed";
+  newElement.style.top = "50%";
+  newElement.style.left = "50%";
+  newElement.style.transform = "translate(-50%, -50%)";
+
+  // 前のカウント要素が存在する場合は削除
+  if (countElement) {
+    countElement.remove();
+  }
+
+  // 新しいカウント要素をbodyに追加
+  document.body.appendChild(newElement);
+
+  // 新しいカウント要素をcountElementに格納して更新
+  countElement = newElement;
+
+  // カウントが1になったら要素を削除
+  if (counts === 1) {
+    countElement.remove();
+    countElement = null; // countElementをリセット
+  }
+});
+
 // ユーザーが切断した時の処理
 socket.on("disconnect", function () {
   // 接続された全てのクライアントに現在の接続数を送信
