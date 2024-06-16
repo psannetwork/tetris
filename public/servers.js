67cda8f991bb95c11bd39cd47e20dda47eb604da
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-16 09:59:46 +0900

    Create servers.js

---
+++ public/servers.js
@@ -0,0 +1,35 @@
+var socket = io();
+var test = "a";
+socket.emit("chat message", test);
+
+socket.on("chat message", function (msg) {
+  console.log(msg);
+  if (msg === "消えたよ") {
+    setkiller();
+  }
+});
+
+function updateUsersCount(count) {
+  var userCountElement = document.getElementById("user-count");
+  userCountElement.textContent = count;
+}
+
+socket.on("connect", function () {
+  socket.emit("new user");
+
+  socket.emit("update users count", socket.io.engine.clientsCount);
+
+  var playerName = "player" + Math.floor(Math.random() * 1000);
+  socket.emit("chat message", "Join a " + playerName);
+});
+
+// 接続されたユーザー数を更新
+socket.on("update users count", function (count) {
+  updateUsersCount(count);
+});
+
+// ユーザーが切断した時の処理
+socket.on("disconnect", function () {
+  // 接続された全てのクライアントに現在の接続数を送信
+  socket.emit("update users count", socket.io.engine.clientsCount - 1);
+});
