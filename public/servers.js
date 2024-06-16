dda1c72c31f9ce0bd27051a83d8e8a0b66c2732b
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-06-17 07:37:52 +0900

    Update servers.js

--- public/servers.js
+++ public/servers.js
@@ -1,6 +1,4 @@
 var socket = io();
-var test = "a";
-socket.emit("chat message", test);
 
 socket.on("chat message", function (msg) {
   console.log(msg);
@@ -9,18 +7,21 @@ socket.on("chat message", function (msg) {
   }
   if (msg === "stop") {
     alert("You win!");
-    window.location.reload();
+    location.reload();
   }
   if (msg === "start") {
     startgames();
   }
 });
+
 socket.on("update users count", (count) => {
   console.log("Total users:", count);
+  updateUsersCount(count);
 });
+
 function updateUsersCount(count) {
   var userCountElement = document.getElementById("user-count");
-  userCountElement.textContent = count;
+  userCountElement.textContent = "Users: " + count;
 }
 
 socket.on("connect", function () {
@@ -32,18 +33,11 @@ socket.on("connect", function () {
   socket.emit("chat message", "Join a " + playerName);
 });
 
-// 接続されたユーザー数を更新
-socket.on("update users count", function (count) {
-  updateUsersCount(count);
-});
-
-// 前のカウント要素を格納するための変数
 let countElement = null;
 
 socket.on("count message", function (counts) {
-  console.log(counts); // デバッグ用にコンソールに表示
+  console.log(counts);
 
-  // 新しいカウント要素を作成
   const newElement = document.createElement("div");
   newElement.textContent = "Please wait... " + counts;
   newElement.style.position = "fixed";
@@ -51,26 +45,53 @@ socket.on("count message", function (counts) {
   newElement.style.left = "50%";
   newElement.style.transform = "translate(-50%, -50%)";
 
-  // 前のカウント要素が存在する場合は削除
   if (countElement) {
     countElement.remove();
   }
 
-  // 新しいカウント要素をbodyに追加
   document.body.appendChild(newElement);
-
-  // 新しいカウント要素をcountElementに格納して更新
   countElement = newElement;
 
-  // カウントが1になったら要素を削除
   if (counts === 1) {
     countElement.remove();
-    countElement = null; // countElementをリセット
+    countElement = null;
   }
 });
 
-// ユーザーが切断した時の処理
 socket.on("disconnect", function () {
-  // 接続された全てのクライアントに現在の接続数を送信
   socket.emit("update users count", socket.io.engine.clientsCount - 1);
+});
+
+function getCurrentTetrisConfiguration() {
+  const configuration = arena.map((row) => row.slice());
+
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        configuration[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+
+  return configuration;
+}
+
+function generateRandomUserId() {
+  return Math.floor(Math.random() * 10 ** 20)
+    .toString()
+    .padStart(20, "0");
+}
+
+var user = generateRandomUserId();
+
+function sendCurrent() {
+  var currents = getCurrentTetrisConfiguration();
+  socket.emit("current", currents, user);
+}
+
+setInterval(sendCurrent, 1000);
+
+socket.on("current", (currents, user) => {
+  console.log(currents);
+  console.log(user);
 });
